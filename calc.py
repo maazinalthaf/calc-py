9d327d391a8ca9b0abe3fea1ce477e43d761d750
moozxdz <76737096+maazinalthaf@users.noreply.github.com>
2022-06-28 00:08:29 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,45 @@
+# This function adds two numbers
+def add(x, y):
+    return x + y
+
+# This function subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# This function multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("a.Add")
+print("b.Subtract")
+print("c.Multiply")
+print("d.Divide")
+
+while True:
+    # take input from the user
+    choice = input("Enter choice(a/b/c/d): ")
+
+    # check if choice is one of the four options
+    if choice in ('a', 'b', 'c', 'd'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == 'a':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == 'b':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == 'c':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == 'd':
+            print(num1, "/", num2, "=", divide(num1, num2))
+    else:
+        print("Invalid Input")
